4205cdc7e09f20a845ba8547c59260fb52c6510c
root <root@xps.21vtech.com>
2017-03-29 15:31:19 +0800

    svn can send email now

--- backup.scripts/svn.py
+++ backup.scripts/svn.py
@@ -11,3 +11,11 @@ from remote_sync import bash_rsync
 bash_rsync(LOG,SRC,DEST)
 from remote_sync import check_log
 check_log(LOG,ERR_FILE)
+
+SUBJECT = 'SVN REPO BACKUP ' + strftime("%x,%X")
+MSG = LOG
+FROM = 'xps@21vtech.com'
+TO = open('/root/john.mail.list',"r").read()
+
+from send_email import send_email
+send_email(FROM,TO,SUBJECT,MSG)
