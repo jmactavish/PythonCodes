a41290e591b95cf60685a452e85187d27bdb380d
root <root@xps.21vtech.com>
2017-03-30 10:42:21 +0800

    privcy info hidden

--- backup.scripts/svn.py
+++ backup.scripts/svn.py
@@ -3,7 +3,7 @@
 from time import strftime
 
 LOG = "/data/dev_svn_backup/logs/" + strftime("%Y%m%d-%H%M") + ".log"
-SRC = "backup_robot@util2.21vtech.com:/var/svn"
+SRC = "backup_robot@util2:/var/svn"
 DEST = "/data/dev_svn_backup/"
 ERR_FILE = "/root/error.msg"
 
