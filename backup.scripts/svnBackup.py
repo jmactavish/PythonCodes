707d021afd535edefc3bc03777ef2a3e47ac17a3
John <backup_robot@xps.21vtech.com>
2017-04-07 15:35:00 +0800

    hide ssh port

--- backup.scripts/svnBackup.py
+++ backup.scripts/svnBackup.py
@@ -15,7 +15,7 @@ with open(Yaml,'r') as yamlFile:
         log = dest + 'logs/' + strftime("%Y%m%d-%H%M") + '.rsync.log'
 	mail = conf['mail']
 
-fullRsync(address['src'],destDir,'22',log)
+fullRsync(address['src'],destDir,address['port'],log)
 
 checkLog(log,conf['err'])
 	
