3e1433f6733be9272f97e526ac9b71ea84da24e7
John <backup_robot@xps.21vtech.com>
2017-04-14 16:01:53 +0800

    monitor wechat

---
+++ backup.scripts/wechatVerify.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import yaml
+import os
+from time import strftime
+from jira import JIRA
+
+Yaml = '/home/backup_robot/PythonCodes/backup.scripts/wechatConf.yml'
+
+with open(Yaml,'r') as yamlFile:
+        conf = yaml.load(yamlFile)
+        address = conf['address']
+	Jira = conf['jira']
+	log = address['dest'] + 'logs/' + strftime("%Y%m%d%H") + '.rsync.log'
+
+jiraServer = JIRA(options={'server':Jira['addr'],'verify':False},basic_auth=(Jira['user'],Jira['pass']))
+
+def sendJira(desc):
+	jiraServer.create_issue(project=Jira['project'],summary=Jira['summary'],description=desc,issuetype={'name':'Bug'},assignee={'name':Jira['assign']})
+
+fileIn = address['dest'] + 'vnet'
+fileOut = fileIn + '-' + strftime("%Y%m%d%H") + '.tar.gz'
+
+if not os.path.exists(log):
+	sendJira(Jira['desc'])
+elif (os.path.getsize(conf['err']) > 0 ):
+	with open(conf['err'],'r') as errors:
+		desc = errors.read()
+		sendJira(desc)
+elif os.path.exists(log):
+	with open(log,'r') as Log:
+		count = 0
+		for line in Log:
+			if 'sql' in line:
+				count += 1
+		print count
+		if not ( count == 1 ):
+			sendJira(Jira['dbErr'])
+elif not os.path.exists(fileOut):
+	sendJira(Jira['noArchive'])
