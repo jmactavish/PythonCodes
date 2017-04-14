b46095e1fddf04ee52c9637948cd81629cfed74e
John <backup_robot@xps.21vtech.com>
2017-04-14 10:16:55 +0800

    changed time stamp, since the job usually take longer than 1 hour

--- backup.scripts/confluenceBackup.py
+++ backup.scripts/confluenceBackup.py
@@ -10,7 +10,7 @@ Yaml = '/home/backup_robot/PythonCodes/backup.scripts/confluenceConf.yml'
 with open(Yaml,'r') as yamlFile:
 	conf = yaml.load(yamlFile)
 	address = conf['address']
-	log = address['dest'] + 'logs/' + strftime("%Y%m%d-%H%M") + '.rsync.log'
+	log = address['dest'] + 'logs/' + strftime("%Y%m%d") + '.rsync.log'
 	mail = conf['mail']
 
 noDelRsync(address['src'],address['dest'],address['port'],log)
---
+++ backup.scripts/confluenceVerify.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import yaml
+import os
+from time import strftime
+from jira import JIRA
+
+Yaml = '/home/backup_robot/PythonCodes/backup.scripts/confluenceConf.yml'
+
+with open(Yaml,'r') as yamlFile:
+        conf = yaml.load(yamlFile)
+        address = conf['address']
+	Jira = conf['jira']
+	log = address['dest'] + 'logs/' + strftime("%Y%m%d") + '.rsync.log'
+
+jiraServer = JIRA(options={'server':Jira['addr'],'verify':False},basic_auth=(Jira['user'],Jira['pass']))
+
+def sendJira(desc):
+	jiraServer.create_issue(project=Jira['project'],summary=Jira['summary'],description=desc,issuetype={'name':'Bug'},assignee={'name':Jira['assign']})
+
+if not os.path.exists(log):
+	sendJira(Jira['desc'])
+elif (os.path.getsize(conf['err']) > 0 ):
+	with open(conf['err'],'r') as errors:
+		desc = errors.read()
+		sendJira(desc)
+else:
+	print('safe')
