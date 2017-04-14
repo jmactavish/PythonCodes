3cca25d35c992a5e9eda532dd6f383403e9ad50b
John <backup_robot@xps.21vtech.com>
2017-04-14 11:26:06 +0800

    new bcakup

---
+++ backup.scripts/wechatBackup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from backupLib import rsync,checkLog,sendMail,tarGz
+from time import strftime
+import os
+import yaml
+
+Yaml = '/home/backup_robot/PythonCodes/backup.scripts/wechatConf.yml'
+
+with open(Yaml,'r') as yamlFile:
+	conf = yaml.load(yamlFile)
+	address = conf['address']
+	log = address['dest'] + 'logs/' + strftime("%Y%m%d%H") + '.rsync.log'
+	mail = conf['mail']
+	source = address['src']
+	sourceList = source['php'] + ' ' + source['db']
+
+rsync(sourceList,address['dest'],address['port'],log)
+
+checkLog(log,conf['err'])
+	
+sendMail(mail['from'],mail['receiver'],mail['summary'],log)
+
+fileIn = address['dest'] + 'vnet'
+fileOut = fileIn + '-' + strftime("%Y%m%d%H") + '.tar.gz'
+
+tarGz(fileIn,fileOut)
