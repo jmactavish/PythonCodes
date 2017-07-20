eb9706a17146156ceb58649f44157110d850671a
John Mactavish <rldjhs@gmial.com>
2017-07-20 11:47:44 +0800

    reformat

--- backup.scripts/confluenceVerify.py
+++ backup.scripts/confluenceVerify.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#pip install pyyaml jira
+# pip install pyyaml jira
 
 import yaml
 import os
@@ -8,22 +8,28 @@ from jira import JIRA
 
 Yaml = 'confluenceConf.yml'
 
-with open(Yaml,'r') as yamlFile:
-        conf = yaml.load(yamlFile)
-        address = conf['address']
-	Jira = conf['jira']
-	log = address['dest'] + 'logs/' + strftime("%Y%m%d") + '.rsync.log'
+with open(Yaml, 'r') as yamlFile:
+    conf = yaml.load(yamlFile)
+    address = conf['address']
+    Jira = conf['jira']
+    log = address['dest'] + 'logs/' + strftime("%Y%m%d") + '.rsync.log'
 
-jiraServer = JIRA(options={'server':Jira['addr'],'verify':False},basic_auth=(Jira['user'],Jira['pass']))
+jiraServer = JIRA(options={'server': Jira['addr'], 'verify': False}, basic_auth=(Jira['user'], Jira['pass']))
+
 
 def sendJira(desc):
-	jiraServer.create_issue(project=Jira['project'],summary=Jira['summary'],description=desc,issuetype={'name':'Bug'},assignee={'name':Jira['assign']})
+    try:
+        jiraServer.create_issue(project=Jira['project'], summary=Jira['summary'], description=desc,
+                                issuetype={'name': 'Bug'}, assignee={'name': Jira['assign']})
+    except:
+        print('sendJira Error!')
+
 
 if not os.path.exists(log):
-	sendJira(Jira['desc'])
-elif (os.path.getsize(conf['err']) > 0 ):
-	with open(conf['err'],'r') as errors:
-		desc = errors.read()
-		sendJira(desc)
+    sendJira(Jira['desc'])
+elif (os.path.getsize(conf['err']) > 0):
+    with open(conf['err'], 'r') as errors:
+        desc = errors.read()
+        sendJira(desc)
 else:
-	print('safe')
+    print('safe')
